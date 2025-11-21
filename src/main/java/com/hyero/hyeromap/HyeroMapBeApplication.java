5d90bdde5e112b3d31b48622969b19266c4ddc04
Junhee <dkfl7190@naver.com>
2025-11-21 16:23:33 +0900

    feat: Initial setup

---
+++ src/main/java/com/hyero/hyeromap/HyeroMapBeApplication.java
@@ -0,0 +1,13 @@
+package com.hyero.hyeromap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeroMapBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyeroMapBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyero/hyeromap/HyeroMapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyero.hyeromap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyeroMapBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
